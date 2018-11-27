3c141527b06577d5040d422f912faf0b4d927d23
Amandreatos <anthony@321webmarketing.com>
2018-11-26 19:29:11 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    pokemon: {}
+  };
+
+  componentDidMount() {
+    this.fetchPokemon();
+  }
+
+  fetchPokemon = () => {
+    const url = "https://pokeapi.co/api/v2/";
+    fetch(url + "pokemon/?count=151")
+      .then(function(response) {
+        return response.json();
+      })
+      .then(myJson => {
+        console.log(JSON.stringify(myJson));
+        this.setState({ pokemon: myJson.results });
+      });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <header className="App-header" />
+        <section className="pokeList">
+          {this.state.pokemon[0] &&
+            this.state.pokemon.map((pokemon, index) => {
+            return (
+              <figure key={index} className="singlePokemon">
+                <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`} alt={pokemon.name} />
+                <h4 className="singlePokemon__name">{pokemon.name}</h4>
+              </figure>
+            )
+          })
+          }
+        </section>
       </div>
     );
   }
